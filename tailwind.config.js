2b1a5845939c196cf80db8128d6efca0d4c8a4f4
Bhargavimachhi <140937880+Bhargavimachhi@users.noreply.github.com>
2024-02-28 22:13:40 +0530

    Boiler Plate

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/index.html',
+    './views/**/*.{html,js}',
+  
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+       "font-family": "Kaushan Script", cursive,
+      }
+    },
+  },
+  plugins: [],
+}
+  
+
