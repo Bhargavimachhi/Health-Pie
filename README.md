b736da3bb3486ae47948aec1329b774f3ecc038b
Jaimin Detroja <officialjaimin345@gmail.com>
2024-07-14 18:02:31 +0530

    update project details in Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# Health-Pie
+## healthPie: A Health & Fitness Management App
+
+The `healthPie` project : a Node.js application for managing your health and fitness goals. 
+
+### Features
+
+* User Authentication (signup and login)
+* Manage Appointments with Doctors
+* Search for Recipes based on Nutritional Needs
+* Track Meals and View Nutritional Information
+
+### Dependencies
+
+This project uses the following dependencies:
+
+* Express - Web framework for building APIs
+* Mongoose - Object Data Modeling (ODM) for MongoDB
+* EJS - Templating engine for generating dynamic HTML
+* Method-Override - HTTP method override middleware
+* dotenv - Load environment variables from a `.env` file
+* Cookie-parser - Parses and signs cookie data
+
+### Installation 
+
+1. Clone this repository.
+2. Install the dependencies using `npm install`. if this show error then run `yarn install`.
+3. Create a `.env` file in the project root directory and add your JWT SECRET KEY:
+
+```
+JWT_SECRET_KEY=YOUR_KEY
+```
+
+### Running the application
+
+1. Start the server by running `npm start` in your terminal.
+2. The application will run on port `8000` by default. You can access it in your browser at `http://localhost:8000`.
+
+### Folder Structure
+
+The project is organized into the following folders:
+
+* `models`: Contains Mongoose models for data structures (e.g., `doctor.js`)
+* `routes`: Contains routes for handling user requests (e.g., `user.js`)
+* `views`: Contains EJS templates for generating HTML pages
+* `public`: Contains static assets like CSS files
+
+### Usage
+
+1. **User Authentication:**
+    * Register a new account using the signup route (`/signup`).
+    * Login to your account using the login route (`/login`).
+2. **Appointments:**
+    * View a list of available Doctors (`/appointments`).
+    * View Appointments for a specific doctor by email (`/appointments/:email`).
+    * Book an appointment with a Doctor by providing your name, message, and contact information (`/appointments/:email/check`).
+3. **Meals:**
+    * Search for recipes based on your nutritional needs (`/mymeals`).
+    * View detailed nutritional information for a specific recipe (`/mymeals/:id/view`).
+4. **Exercise and Tasks:**
+    * Track your exercise routines (`/exercise`).
+    * Manage your daily tasks (`/task`). 
--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import path from "path";
 import methodOverride from "method-override";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-const port=8000;
-const app=express();
+const port = 8000;
+const app = express();
 
 import userRoute from "./routes/user.js";
 import doctorRoute from "./routes/doctor.js";
@@ -15,178 +15,178 @@ import doctor from "./models/doctor.js";
 
 
 dotenv.config();
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 // app.set("views",path.join(__dirname,"/views"));
 
 app.use(express.static("views"));
-app.use(express.urlencoded({extended :true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(cookieParser());
 
-app.use("/user",userRoute);
-app.use("/doc",doctorRoute);
-app.use("/signup",signupRoute);
-app.use("/login",loginRoute);
+app.use("/user", userRoute);
+app.use("/doc", doctorRoute);
+app.use("/signup", signupRoute);
+app.use("/login", loginRoute);
 
-async function main(){
+async function main() {
     mongoose.connect("mongodb://127.0.0.1:27017/healthPie");
 }
 
-main().then(()=>{
+main().then(() => {
     console.log("MongoDB connection successfull");
-}).catch((err)=>{
-    console.log("Failure");
+}).catch((err) => {
+    console.log("Failure in connection of database ");
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Server Started");
 });
 
-app.get("/public/style.css", function(req, res) {
-    res.sendFile("style.css",{root : "public"});
+app.get("/public/style.css", function (req, res) {
+    res.sendFile("style.css", { root: "public" });
 });
 
-app.get("/",(req,res)=>{
-    res.sendFile("index.html",{root :  "views"});
+app.get("/", (req, res) => {
+    res.sendFile("index.html", { root: "views" });
 })
 
-app.get("/login",(req,res)=>{
-    res.sendFile("login.html",{root : "views"});
+app.get("/login", (req, res) => {
+    res.sendFile("login.html", { root: "views" });
 });
 
-app.get("/signup",(req,res)=>{
-    res.sendFile("signup.html",{root : "views"});
+app.get("/signup", (req, res) => {
+    res.sendFile("signup.html", { root: "views" });
 });
 
-app.get("/mymeals",(req,res)=>{
-    res.sendFile("mymeals.html",{root : "views"});
+app.get("/mymeals", (req, res) => {
+    res.sendFile("mymeals.html", { root: "views" });
 })
 
-app.get("/mymeals/view",(req,res)=>{
-    let q=`https://api.spoonacular.com/recipes/findByNutrients?minVitaminB12=1&apiKey=093f59c223dc4b0b8a63411d7de1dafd`;
+app.get("/mymeals/view", (req, res) => {
+    let q = `https://api.spoonacular.com/recipes/findByNutrients?minVitaminB12=1&apiKey=093f59c223dc4b0b8a63411d7de1dafd`;
     fetch(`${q}`)
-    .then(response => {
-        if (!response.ok) {
-            let code=201;
-            let msg="Invalid Responce";
-            let description="";
-    
-            res.render("error.ejs",{code,msg,description});
-        }
-        return response.json();
-    })
-    .then(userData => {
-        res.render("mymeals.ejs",{userData});
-    })
-    .catch(error => {
-        let code=201;
-        let msg="No Food Available";
-        let description="";
+        .then(response => {
+            if (!response.ok) {
+                let code = 201;
+                let msg = "Invalid Responce";
+                let description = "";
 
-        res.render("error.ejs",{code,msg,description});
-    });
+                res.render("error.ejs", { code, msg, description });
+            }
+            return response.json();
+        })
+        .then(userData => {
+            res.render("mymeals.ejs", { userData });
+        })
+        .catch(error => {
+            let code = 201;
+            let msg = "No Food Available";
+            let description = "";
+
+            res.render("error.ejs", { code, msg, description });
+        });
 })
 
-app.post("/mymeals/view",(req,res)=>{
-    let q=`https://api.spoonacular.com/recipes/findByNutrients?`;
+app.post("/mymeals/view", (req, res) => {
+    let q = `https://api.spoonacular.com/recipes/findByNutrients?`;
     for (const [key, value] of Object.entries(req.body)) {
-        if(value=== '' || value===' ' || value==null || value==undefined){
+        if (value === '' || value === ' ' || value == null || value == undefined) {
             continue;
         }
-        q+=`${key}=${value}&`;
+        q += `${key}=${value}&`;
     }
-    q+=`apiKey=093f59c223dc4b0b8a63411d7de1dafd`;
+    q += `apiKey=093f59c223dc4b0b8a63411d7de1dafd`;
     fetch(`${q}`)
-    .then(response => {
-        if (!response.ok) {
-            let code=201;
-            let msg="Invalid Responce";
-            let description="";
-    
-            res.render("error.ejs",{code,msg,description});
-        }
-        return response.json();
-    })
-    .then(userData => {
-        if(userData.length==0){
-            let code=201;
-            let msg="No Food Available";
-            let description="";
+        .then(response => {
+            if (!response.ok) {
+                let code = 201;
+                let msg = "Invalid Responce";
+                let description = "";
 
-            res.render("error.ejs",{code,msg,description});
-        }
-        else{
-            res.render("mymeals.ejs",{userData});
-        }
-        
-    })
-    .catch(error => {
-        let code=201;
-        let msg="No Food Available";
-        let description="";
+                res.render("error.ejs", { code, msg, description });
+            }
+            return response.json();
+        })
+        .then(userData => {
+            if (userData.length == 0) {
+                let code = 201;
+                let msg = "No Food Available";
+                let description = "";
 
-        res.render("error.ejs",{code,msg,description});
-    });
+                res.render("error.ejs", { code, msg, description });
+            }
+            else {
+                res.render("mymeals.ejs", { userData });
+            }
+
+        })
+        .catch(error => {
+            let code = 201;
+            let msg = "No Food Available";
+            let description = "";
+
+            res.render("error.ejs", { code, msg, description });
+        });
 });
 
-app.get("/mymeals/:id/view",(req,res)=>{
-    let {id}=req.params;
-    let q=`https://api.spoonacular.com/recipes/${id}/nutritionWidget.json?apiKey=093f59c223dc4b0b8a63411d7de1dafd`;
+app.get("/mymeals/:id/view", (req, res) => {
+    let { id } = req.params;
+    let q = `https://api.spoonacular.com/recipes/${id}/nutritionWidget.json?apiKey=093f59c223dc4b0b8a63411d7de1dafd`;
     // console.log(q);
     fetch(`${q}`)
-    .then(response => {
-        if (!response.ok) {
-            let code=201;
-            let msg="Invalid Responce";
-            let description="";
-    
-            res.render("error.ejs",{code,msg,description});
-        }
-        return response.json();
-    })
-    .then(data => {
-        let bad=data.bad;
-        let good=data.good;
-        res.render("viewMeal.ejs",{bad , good});
-    })
-    .catch(error => {
-        let code=201;
-        let msg="No Food Available";
-        let description="";
+        .then(response => {
+            if (!response.ok) {
+                let code = 201;
+                let msg = "Invalid Responce";
+                let description = "";
 
-        res.render("error.ejs",{code,msg,description});
-    });
+                res.render("error.ejs", { code, msg, description });
+            }
+            return response.json();
+        })
+        .then(data => {
+            let bad = data.bad;
+            let good = data.good;
+            res.render("viewMeal.ejs", { bad, good });
+        })
+        .catch(error => {
+            let code = 201;
+            let msg = "No Food Available";
+            let description = "";
+
+            res.render("error.ejs", { code, msg, description });
+        });
 });
 
-app.get("/exercise",(req,res)=>{
-    res.sendFile("exercise.html",{root : "views"});
+app.get("/exercise", (req, res) => {
+    res.sendFile("exercise.html", { root: "views" });
 });
 
-app.get("/task",(req,res)=>{
-    res.sendFile("cal.html",{root : "views"});
+app.get("/task", (req, res) => {
+    res.sendFile("cal.html", { root: "views" });
 });
 
-app.get("/appointments",async(req,res)=>{
-    let docData=await doctor.find({});
-    let doc=[];
-    res.render("viewAppoinment.ejs",{doc ,docData});
+app.get("/appointments", async (req, res) => {
+    let docData = await doctor.find({});
+    let doc = [];
+    res.render("viewAppoinment.ejs", { doc, docData });
 });
 
-app.get("/appointments/:email",async(req,res)=>{
-    let data=await doctor.find({email : req.params.email});
-    res.render("appointments.ejs",{data});
+app.get("/appointments/:email", async (req, res) => {
+    let data = await doctor.find({ email: req.params.email });
+    res.render("appointments.ejs", { data });
 });
 
-app.get("/doctor/:email",async(req,res)=>{
-    let data=await doctor.find({email : req.params.email});
+app.get("/doctor/:email", async (req, res) => {
+    let data = await doctor.find({ email: req.params.email });
     res.render("doctorDash.ejs");
 });
 
-app.post("/appointments/:email/check",async (req,res)=>{
-    let doc=await doctor.find({email : req.params.email});
-    let docData=await doctor.find({});
+app.post("/appointments/:email/check", async (req, res) => {
+    let doc = await doctor.find({ email: req.params.email });
+    let docData = await doctor.find({});
 
-    doc[0].appoinments.push({"name" : req.body.name , "msg" : req.body.msg , "contact":req.body.contact});
-    res.render("viewAppoinment.ejs",{doc : doc[0].appoinments, docData});
+    doc[0].appoinments.push({ "name": req.body.name, "msg": req.body.msg, "contact": req.body.contact });
+    res.render("viewAppoinment.ejs", { doc: doc[0].appoinments, docData });
 })
